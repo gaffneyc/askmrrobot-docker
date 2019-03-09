429afb8c84726b01bbb9b92f3b3587bbd83d0d77
Chris Gaffney <gaffneyc@gmail.com>
2019-03-09 00:03:24 -0500

    Adds script to automatically push updated version
    This is currently running every hour inside a cron job (monitored by
    Dead Man's Snitch - https://deadmanssnitch.com [end shameless plug])

---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+current="$(grep -Poe 'any-\K\d+' Dockerfile)"
+upstream="$(curl -s https://www.askmrrobot.com/wow/simulator/client | grep -Poe 'Version \K(\d)+')"
+
+if [ $current != $upstream ]; then
+  echo "current:$current upstream:$upstream"
+
+  # Update the version references in the description and download URL.
+  # sed -i "s/$current/$upstream/g" Dockerfile
+  sed -i "s/\([v|any-]\)$current/\1$upstream/" Dockerfile
+  git commit Dockerfile -m "Update client to version $upstream"
+
+  git push
+fi
